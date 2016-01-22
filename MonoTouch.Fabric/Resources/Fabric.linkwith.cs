53c100031505760ddc7bc880713533e3a23a9067
Matthew Leibowitz <mattleibow@live.com>
2016-01-22 23:21:14 +0200

    Add the security framework

--- MonoTouch.Fabric/Resources/Fabric.linkwith.cs
+++ MonoTouch.Fabric/Resources/Fabric.linkwith.cs
@@ -4,4 +4,5 @@ using ObjCRuntime;
 	"Fabric.a", 
 	LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.Arm64, 
 	SmartLink = true,
-	ForceLoad = true)]
+	ForceLoad = true,
+	Frameworks = "Security")]
